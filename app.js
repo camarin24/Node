083200480d515172af8d06ca7356a9261d18805a
Cristian Marin <Cristian Marin>
2016-06-05 15:34:47 -0500

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var app = express();
+app.set("view engine","jade");
+app.get("/",function(req,res){
+  res.render("index",{tata:"Tatiana"});
+})
+app.listen(8081)
